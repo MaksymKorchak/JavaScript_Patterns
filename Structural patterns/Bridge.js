d26fedbce1c23289169ab092d1f222d916519f45
Maksym Korchak <mkorchak1992@gmail.com>
2022-01-25 18:45:56 +0200

    Add Bridge pattern

---
+++ Structural patterns/Bridge.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class Model {
+    constructor(color) {
+        this.color = color;
+    }
+}
+
+class Color {
+    constructor(type) {
+        this.type = type;
+    }
+    get() {
+        return this.type;
+    }
+}
+
+class BlackColor extends Color {
+    constructor() {
+        super("dark-black");
+    }
+};
+
+class SilbrigColor extends Color {
+    constructor() {
+        super("Silbermetallic")
+    }
+};
+
+
+class Audi extends Model {
+    constructor(color) {
+        super(color);
+    }
+    paint() {
+        return `Auto: Audi, Color ${this.color.get()}`;
+    }
+};
+
+class Bmw extends Model {
+    constructor(color) {
+        super(color);
+    }
+    paint() {
+        return `Auto: Bmw, Color ${this.color.get()}`;
+    }
+};
+
+const blackAudi = new Audi(new BlackColor());
+console.log(blackAudi.paint());
